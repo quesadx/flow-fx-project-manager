0d71b134e2527fd65301775f4ec1a740a11661c6
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-05-14 00:39:24 -0600

    feat: add flake.nix for FlowFX Java/Maven project setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Java template: Maven + JavaFX";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      jdk = pkgs.jdk21.override { enableJavaFX = true; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          jdk
+          maven
+          javaPackages.openjfx21
+        ];
+
+        shellHook = ''
+          export JAVA_HOME="${jdk}"
+        '';
+      };
+    };
+}
